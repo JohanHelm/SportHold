ceb94cf64fa542431d80d078e792a7eb6c2babff
John Doe <johndoe@example.com>
2023-11-14 10:32:54 +0300

    remove unused const from DAL

--- app/infra/db/pgdb/dal.py
+++ app/infra/db/pgdb/dal.py
@@ -4,10 +4,6 @@ from sqlalchemy import Engine, event
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
-SYNC_DATABASE_URL = "postgresql+psycopg2://postgres:qwerty123@127.0.0.1:5432/dev"
-ASYNC_DATABASE_URL = "postgresql+asyncpg:///postgres:qwerty123@127.0.0.1:5432/dev"
-
-
 class Builder:
     def __init__(self, uri: str, echo: bool):
         self.engine = create_async_engine(url=uri, echo=echo)
