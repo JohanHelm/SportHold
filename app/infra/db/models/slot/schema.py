4895399231f82c9e115fe5252e93d3eaaf234feb
John Doe <johndoe@example.com>
2023-11-23 12:12:05 +0300

    added TYPE_CHEKING import for slot schema

--- app/infra/db/models/slot/schema.py
+++ app/infra/db/models/slot/schema.py
@@ -1,9 +1,13 @@
-from typing import List
+from typing import TYPE_CHECKING, List
 from sqlalchemy import Integer, ForeignKey, DateTime, String
 from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.orm import mapped_column
 
 from ...models import Base
+
+if TYPE_CHECKING:
+    from record.schema import Record
+    from schedule.schema import Schedule
 
 
 class Slot(Base):
@@ -20,10 +24,12 @@ class Slot(Base):
     schedule: Mapped["Schedule"] = relationship(back_populates="slots")
 
     def __str__(self):
-        return f"SQLA Slot, " \
-               f"id: {self.id}," \
-               f" schedule: {self.schedule.id}," \
-               f" records: {[str(x) for x in self.record]}," \
-               f" start at: {self.started_at}," \
-               f" duration: {self.duration}," \
-               f" status: {self.status}"
+        return (
+            f"SQLA Slot, "
+            f"id: {self.id},"
+            f" schedule: {self.schedule.id},"
+            f" records: {[str(x) for x in self.record]},"
+            f" start at: {self.started_at},"
+            f" duration: {self.duration},"
+            f" status: {self.status}"
+        )
