51bac818a68bb57ab177469fd200e7d39eea3509
aa.bezkrovnyy <aa.bezkrovnyy@jet.su>
2023-11-24 23:26:07 +0300

    some TODO

--- app/domain/models/schedule/dto.py
+++ app/domain/models/schedule/dto.py
@@ -11,6 +11,7 @@ class ScheduleStatus(Enum):
     ACTIVE = auto()
     NOT_ACTIVE = auto()
 
+# TODO: move to slot dto and then import 
 class SlotType(Enum):
     ACCESSEBLE = auto()
     RESTRICTED = auto()
@@ -22,25 +23,32 @@ class SuggestPolicy(Enum):
     REGULAR = auto()
 
 
-
+# вся киллер фича - тут
 class ScheduleBase(BaseModel):
+    # TODO: truthy BASE
     name: Optional[str] = Field(default=None)
     description: Optional[str] = Field(default=None)
     status: Optional[ScheduleStatus] = Field(default=ScheduleStatus.NOT_ACTIVE)
+    # TODO: change for another data struct? 
     valid_from: Optional[date] = Field(default=date.today())
     valid_to: Optional[str] = Field(default=date.today() + timedelta(days=30))
+    # TODO: array of masks?
     mask_weekdays: Optional[DaysOfWeek] = Field(default=DaysOfWeek.ALL) 
     mask_weeks: Optional[WeeksInYear] = Field(default=WeeksInYear.ALL)
     mask_quratals: Optional[Quartals] = Field(default=Quartals.ALL)
     mask_days_month: Optional[DaysInMonth] = Field(default=None)
+    # TODO: array of nth weekdays index?
     nth_weekday: Optional[DaysOfWeek] = Field(default=None)
     nth_index: Optional[int] = Field(None, ge=1, le=31)
+    # TODO: change for another data struct? 
     slot_type: Optional[SlotType] = Field(default=SlotType.ACCESSEBLE)
     slot_min_time: Optional[int] = Field(None, ge=1, le=1440)
     slot_max_time: Optional[int] = Field(None, ge=1, le=1440)
     slot_step_time: Optional[int] = Field(None, ge=1, le=1440)
+    # TODO: change for another data struct? 
     hour_start: Optional[int] = Field(None, ge=0, le=24)
     hour_end: Optional[int] = Field(None, ge=0, le=24)
+    # TODO: policy array?
     policy_merge: Optional[str] = Field(default=MergePolicy.REGULAR)
     policy_suggest: Optional[str] = Field(default=SuggestPolicy.REGULAR)
     
