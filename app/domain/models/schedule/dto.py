1db73f1142e32f60973d6803906e1f09a3fd1e2d
aa.bezkrovnyy <aa.bezkrovnyy@jet.su>
2023-11-24 17:27:38 +0300

    added default values fro schedule model

--- app/domain/models/schedule/dto.py
+++ app/domain/models/schedule/dto.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 from typing import Optional
 from datetime import date
 from app.helpers.maskers.weekdays import DaysOfWeek
@@ -7,25 +7,25 @@ from app.helpers.maskers.quartals import Quartals
 from app.helpers.maskers.daysmonth import DaysInMonth
 
 class ScheduleBase(BaseModel):
-    name: Optional[str]
-    description: Optional[str]
-    status: str
-    valid_from: date
-    valid_to: date
-    mask_weekdays: DaysOfWeek
-    mask_weeks: WeeksInYear
-    mask_quratals: Quartals
-    mask_days_month: DaysInMonth
-    nth_weekday: DaysOfWeek
-    nth_index: int
-    slot_type: str
-    slot_min_time: int
-    slot_max_time: int
-    slot_step_time: int
-    hour_start: int
-    hour_end: int
-    policy_merge: str
-    policy_suggest: str
+    name: Optional[str] = Field(default=None)
+    description: Optional[str] = Field(default=None)
+    status: Optional[str] = Field(default=None)
+    valid_from: Optional[date] = Field(default=None)
+    valid_to: Optional[str] = Field(default=None)
+    mask_weekdays: Optional[DaysOfWeek] = Field(default=DaysOfWeek.ALL) 
+    mask_weeks: Optional[WeeksInYear] = Field(default=WeeksInYear.ALL)
+    mask_quratals: Optional[Quartals] = Field(default=Quartals.ALL)
+    mask_days_month: Optional[DaysInMonth] = Field(default=None)
+    nth_weekday: Optional[DaysOfWeek] = Field(default=None)
+    nth_index: Optional[int] = Field(None, ge=1, le=31)
+    slot_type: Optional[str] = Field(default=None)
+    slot_min_time: Optional[int] = Field(None, ge=1, le=1440)
+    slot_max_time: Optional[int] = Field(None, ge=1, le=1440)
+    slot_step_time: Optional[int] = Field(None, ge=1, le=1440)
+    hour_start: Optional[int] = Field(None, ge=0, le=24)
+    hour_end: Optional[int] = Field(None, ge=0, le=24)
+    policy_merge: Optional[str] = Field(default="REGULAR")
+    policy_suggest: Optional[str] = Field(default="REGULAR")
     
     rental_id: int
 
