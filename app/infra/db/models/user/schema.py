ecf76186f46313d960fc615a931a841a9dd54390
aa.bezkrovnyy <aa.bezkrovnyy@jet.su>
2023-12-26 13:51:31 +0300

    updated user model

--- app/infra/db/models/user/schema.py
+++ app/infra/db/models/user/schema.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import TYPE_CHECKING, List
 from sqlalchemy import Integer, String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
@@ -5,32 +6,29 @@ from sqlalchemy.orm import relationship
 
 from ...models import Base
 
-from app.domain.models.user.dto import UserRole
-
 if TYPE_CHECKING:
     from ..record.schema import Record
+
 
 class User(Base):
     __tablename__ = "users"
 
-    user_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
     username: Mapped[str] = mapped_column(String)
     fullname: Mapped[str] = mapped_column(String)
-    lang_code: Mapped[str] = mapped_column(String)
-    registration_date: Mapped[DateTime] = mapped_column(DateTime)
-    active: Mapped[int] = mapped_column(Integer, default=1)
-    roles: Mapped["UserRole"] = mapped_column(Integer, default=1)
-    records: Mapped[List["Record"]] = relationship()
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=date.today())
+    is_active: Mapped[int] = mapped_column(Integer, default=1)
+    roles: Mapped[int] = mapped_column(Integer, default=0)
 
+    records: Mapped[List["Record"]] = relationship(back_populates="user")
 
     def __str__(self):
         return (
             f"SQLA User,"
-            f" user_id: {self.user_id},"
-            f" username: {self.username},"
-            f" fullname: {self.fullname},"
-            f" lang_code: {self.lang_code},"
-            f" registartion date: {self.registration_date},"
-            f" active: {self.active},"
-            f" active records count: {len(self.records)}"
+            f"id: {self.id},"
+            f"username: {self.username},"
+            f"fullname: {self.fullname},"
+            f"created_at: {self.created_at},"
+            f"is_active: {self.is_active}"
         )
