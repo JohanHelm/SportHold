b8e9a700dc855edd10c8e0f4bc8968d2bc2d484b
aa.bezkrovnyy <aa.bezkrovnyy@jet.su>
2023-12-26 13:14:00 +0300

    updated record db schema

--- app/infra/db/models/record/schema.py
+++ app/infra/db/models/record/schema.py
@@ -1,5 +1,5 @@
 from typing import TYPE_CHECKING
-from sqlalchemy import UniqueConstraint, ForeignKey
+from sqlalchemy import DateTime, UniqueConstraint, ForeignKey
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
@@ -9,17 +9,21 @@ from ...models import Base
 if TYPE_CHECKING:
     from ..user.schema import User
     from ..slot.schema import Slot
+    from ..rental.schema import Rental
 
 class Record(Base):
     __tablename__ = "records"
 
-    record_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"), unique=False)
-    slot_id: Mapped[int] = mapped_column(ForeignKey("slots.slot_id"), unique=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=False)
+    slot_id: Mapped[int] = mapped_column(ForeignKey("slots.id"), unique=False)
+    rental_id: Mapped[int] = mapped_column(ForeignKey("rentals.id"), unique=False)
+
+    created_at: Mapped[DateTime] = mapped_column(DateTime)
+
     user: Mapped["User"] = relationship(back_populates="records")
-    slot: Mapped["Slot"] = relationship(back_populates="record")
+    slot: Mapped["Slot"] = relationship(back_populates="records")
+    rental: Mapped["Rental"] = relationship(back_populates="records")
 
     __table_args__ = (UniqueConstraint("user_id", "slot_id"),)
-
-    def __str__(self):
-        return f"SQLA Record, id: {self.record_id}, user: {self.user.user_id}, slot: {self.slot.slot_id}"
