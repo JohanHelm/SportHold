801661f217136b837c362a10e6d0ec31036fb8fa
aa.bezkrovnyy <aa.bezkrovnyy@jet.su>
2023-12-28 19:01:02 +0300

    some comments

--- app/telegram/handlers/regular_user_handlers.py
+++ app/telegram/handlers/regular_user_handlers.py
@@ -8,12 +8,17 @@ from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
-from app.telegram.keyboards.regular_user_kb import \
-    create_rental_pagination_keyboard, \
-    create_slot_pagination_keyboard,\
-    create_first_regular_keyboard
+from app.telegram.keyboards.regular_user_kb import (
+    create_rental_pagination_keyboard,
+    create_slot_pagination_keyboard,
+    create_first_regular_keyboard,
+)
 from app.domain.controllers.slots import SlotManager
-from app.telegram.messages.text_messages import display_rental_info, display_booking_info, hello_regular_user
+from app.telegram.messages.text_messages import (
+    display_rental_info,
+    display_booking_info,
+    hello_regular_user,
+)
 from app.infra.db.models.rental.schema import Rental
 from app.domain.models.slot.dto import SlotModel, SlotData
 
@@ -25,37 +30,63 @@ class ShowRentalSlots(StatesGroup):
     choosing_slot_page = State()
 
 
-@router.callback_query(F.data == 'show_rentals', StateFilter(None))
+@router.callback_query(F.data == "show_rentals", StateFilter(None))
 async def show_rentals(callback: CallbackQuery, state: FSMContext, db_session):
     await state.set_state(ShowRentalSlots.choosing_rental_number)
     rental_number = 0
     await state.update_data(choosing_rental_number=rental_number)
+    # TODO: предлагаю пагинацию делать через offset\limit в запросе с order_by по rental.id
+    # TODO: т.е. мы отсортировываем по возрастанию ID и с помощью оффсета смещаем SELECT
+    # TODO: таким образом у нас не нарушится порядок, в котором мы показываем объекты
+    # TODO: соот-но в состояние можно хранить инфо про текущий offset и count объектов
     async with db_session() as session:
-        result = await session.execute(select(Rental).options(selectinload(Rental.schedules)))
+        # TODO: Объектов в базе может и не быть, необходимо эту логику проработать
+        result = await session.execute(
+            select(Rental).options(selectinload(Rental.schedules))
+        )
         rentals = result.scalars().all()
+        # TODO: а что если объект добавлен, но у него нет ни одного расписания или ни одного активного расписания?
+        # TODO: Или нет расписаний, которые на данный момент действуют - started < now() < ended ?
         rentals_schedules = rentals[rental_number].schedules
     # TODO Переписать display_rental_info чтобы принимала список расписаний и выводила рабочее время и перерывы.
-    await callback.message.edit_text(text=display_rental_info(rentals[rental_number], rentals_schedules[0]),
-                                     reply_markup=create_rental_pagination_keyboard(rental_number + 1, len(rentals)))
+    # TODO тут должна быть какая то логика, если активных расписаний нет
+    # TODO предполагаю, что в display_rental_info уйдеи список активных расписаний, действующих на текущий момент
+    await callback.message.edit_text(
+        text=display_rental_info(rentals[rental_number], rentals_schedules[0]),
+        reply_markup=create_rental_pagination_keyboard(rental_number + 1, len(rentals)),
+    )
 
 
-@router.callback_query(F.data.startswith('shift_show_rentals'), StateFilter(ShowRentalSlots.choosing_rental_number))
+@router.callback_query(
+    F.data.startswith("shift_show_rentals"),
+    StateFilter(ShowRentalSlots.choosing_rental_number),
+)
 async def shift_show_rentals(callback: CallbackQuery, state: FSMContext, db_session):
-    rental_number = (await state.get_data())['choosing_rental_number'] + int(callback.data.split('/')[1])
+    rental_number = (await state.get_data())["choosing_rental_number"] + int(
+        callback.data.split("/")[1]
+    )
     await state.update_data(choosing_rental_number=rental_number)
     async with db_session() as session:
-        result = await session.execute(select(Rental).options(selectinload(Rental.schedules)))
+        result = await session.execute(
+            select(Rental).options(selectinload(Rental.schedules))
+        )
         rentals = result.scalars().all()
         rentals_schedules = rentals[rental_number].schedules
-    await callback.message.edit_text(text=display_rental_info(rentals[rental_number], rentals_schedules[0]),
-                                     reply_markup=create_rental_pagination_keyboard(rental_number + 1, len(rentals)))
+    await callback.message.edit_text(
+        text=display_rental_info(rentals[rental_number], rentals_schedules[0]),
+        reply_markup=create_rental_pagination_keyboard(rental_number + 1, len(rentals)),
+    )
 
 
-@router.callback_query(F.data == 'new_booking', StateFilter(ShowRentalSlots.choosing_rental_number))
+@router.callback_query(
+    F.data == "new_booking", StateFilter(ShowRentalSlots.choosing_rental_number)
+)
 async def show_rentals_slots(callback: CallbackQuery, state: FSMContext, db_session):
-    rental_number = (await state.get_data())['choosing_rental_number']
+    rental_number = (await state.get_data())["choosing_rental_number"]
     async with db_session() as session:
-        result = await session.execute(select(Rental).options(selectinload(Rental.schedules)))
+        result = await session.execute(
+            select(Rental).options(selectinload(Rental.schedules))
+        )
         rentals = result.scalars().all()
         rentals_schedules = rentals[rental_number].schedules
     date = datetime.today()
@@ -68,20 +99,34 @@ async def show_rentals_slots(callback: CallbackQuery, state: FSMContext, db_sess
     slots_per_page = 4
     start_slice = slot_page * slots_per_page
     end_slice = start_slice + slots_per_page
-    await callback.message.edit_text(text=display_booking_info(rentals_schedules[0]),
-                                     reply_markup=create_slot_pagination_keyboard(slots.slots[start_slice:end_slice],
-                                                                                  slot_page, len(slots.slots),
-                                                                                  slots_per_page))
+    await callback.message.edit_text(
+        text=display_booking_info(rentals_schedules[0]),
+        reply_markup=create_slot_pagination_keyboard(
+            slots.slots[start_slice:end_slice],
+            slot_page,
+            len(slots.slots),
+            slots_per_page,
+        ),
+    )
 
 
-@router.callback_query(F.data.startswith('shift_show_slots'), StateFilter(ShowRentalSlots.choosing_slot_page))
-async def shift_show_rentals_slots(callback: CallbackQuery, state: FSMContext, db_session):
-    slot_page = (await state.get_data())['choosing_slot_page'] + int(callback.data.split('/')[1])
+@router.callback_query(
+    F.data.startswith("shift_show_slots"),
+    StateFilter(ShowRentalSlots.choosing_slot_page),
+)
+async def shift_show_rentals_slots(
+    callback: CallbackQuery, state: FSMContext, db_session
+):
+    slot_page = (await state.get_data())["choosing_slot_page"] + int(
+        callback.data.split("/")[1]
+    )
     await state.update_data(choosing_slot_page=slot_page)
     async with db_session() as session:
-        result = await session.execute(select(Rental).options(selectinload(Rental.schedules)))
+        result = await session.execute(
+            select(Rental).options(selectinload(Rental.schedules))
+        )
         rentals = result.scalars().all()
-        rental_number = (await state.get_data())['choosing_rental_number']
+        rental_number = (await state.get_data())["choosing_rental_number"]
         rentals_schedules = rentals[rental_number].schedules
     date = datetime.today()
     manager = SlotManager()
@@ -89,10 +134,15 @@ async def shift_show_rentals_slots(callback: CallbackQuery, state: FSMContext, d
     slots_per_page = 4
     start_slice = slot_page * slots_per_page
     end_slice = start_slice + slots_per_page
-    await callback.message.edit_text(text=display_booking_info(rentals_schedules[0]),
-                                     reply_markup=create_slot_pagination_keyboard(slots.slots[start_slice:end_slice],
-                                                                                  slot_page, len(slots.slots),
-                                                                                  slots_per_page))
+    await callback.message.edit_text(
+        text=display_booking_info(rentals_schedules[0]),
+        reply_markup=create_slot_pagination_keyboard(
+            slots.slots[start_slice:end_slice],
+            slot_page,
+            len(slots.slots),
+            slots_per_page,
+        ),
+    )
 
 
 # @router.callback_query(F.data.startswith('sign_up_to_slot'), StateFilter(ShowRentalSlots.choosing_slot_number))
@@ -115,24 +165,34 @@ async def shift_show_rentals_slots(callback: CallbackQuery, state: FSMContext, d
 #     await callback.message.edit_text(text=str(user_id))
 
 
-@router.callback_query(F.data.startswith('back_to_rentals'), StateFilter(ShowRentalSlots.choosing_slot_page))
+@router.callback_query(
+    F.data.startswith("back_to_rentals"),
+    StateFilter(ShowRentalSlots.choosing_slot_page),
+)
 async def back_to_rentals(callback: CallbackQuery, state: FSMContext, db_session):
-    rental_number = (await state.get_data())['choosing_rental_number']
+    rental_number = (await state.get_data())["choosing_rental_number"]
     await state.set_state(ShowRentalSlots.choosing_rental_number)
     async with db_session() as session:
-        result = await session.execute(select(Rental).options(selectinload(Rental.schedules)))
+        result = await session.execute(
+            select(Rental).options(selectinload(Rental.schedules))
+        )
         rentals = result.scalars().all()
         rentals_schedules = rentals[rental_number].schedules
-    await callback.message.edit_text(text=display_rental_info(rentals[rental_number], rentals_schedules[0]),
-                                     reply_markup=create_rental_pagination_keyboard(rental_number + 1, len(rentals)))
+    await callback.message.edit_text(
+        text=display_rental_info(rentals[rental_number], rentals_schedules[0]),
+        reply_markup=create_rental_pagination_keyboard(rental_number + 1, len(rentals)),
+    )
 
 
-@router.callback_query(F.data == 'to_main_menu')
+@router.callback_query(F.data == "to_main_menu")
 async def to_main_menu(callback: CallbackQuery, state: FSMContext, db_session):
     await state.clear()
     avalable_rentals: int = 2
     total_rentals: int = 2
     records_amount: int = 0
     await callback.message.edit_text(
-        hello_regular_user(callback.from_user.username, avalable_rentals, total_rentals, records_amount),
-        reply_markup=create_first_regular_keyboard())
+        hello_regular_user(
+            callback.from_user.username, avalable_rentals, total_rentals, records_amount
+        ),
+        reply_markup=create_first_regular_keyboard(),
+    )
--- app/telegram/messages/text_messages.py
+++ app/telegram/messages/text_messages.py
@@ -2,45 +2,69 @@ from app.infra.db.models.rental.schema import Rental
 from app.infra.db.models.schedule.schema import Schedule
 from app.domain.controllers.slots import SlotData
 
-def hello_regular_user(user_name: str, avalable_rentals: int, total_rentals: int, records_amount: int) -> str:
-    return f"Вам доступно объектов - {avalable_rentals} из {total_rentals}.\n" \
-           f"Найдено ваших записей - {records_amount}.\n" \
-           f"Активная запись\n" \
-           f"Ближайшая запись\n"
+
+def hello_regular_user(
+    user_name: str, avalable_rentals: int, total_rentals: int, records_amount: int
+) -> str:
+    return (
+        f"Вам доступно объектов - {avalable_rentals} из {total_rentals}.\n"
+        f"Найдено ваших записей - {records_amount}.\n"
+        f"Активная запись\n"
+        f"Ближайшая запись\n"
+    )
+
 
 def hello_owner_user(user_name: str) -> str:
-    return f"Привет, {user_name}!\n" \
-           f"Здесь ты можешь создавать объекты и их распсания, просматривать записи клиентов, пополнять счёт и т.д."
+    return (
+        f"Привет, {user_name}!\n"
+        f"Здесь ты можешь создавать объекты и их распсания, просматривать записи клиентов, пополнять счёт и т.д."
+    )
 
 
 # def display_rental_info(rental: Rental) -> str:
 #     return f"Наименование: {rental.name}\n" \
 #            f"Описание: {rental.description}"
 
+
 # TODO в базе schedule.hour_start и schedule.hour_end должны быть datetime или time
 def display_rental_info(rental: Rental, schedule: Schedule) -> str:
     # schedule_start_time = schedule.hour_start.strftime("%H:%M")
     # schedule_end_time = schedule.hour_end.strftime("%H:%M")
-    return f"Наименование: {rental.name}\n" \
-           f"Описание: {rental.description}\n" \
-           f"Расписание:\n" \
-           f"       Время работы: {schedule.started} - {schedule.ended}\n" \
-           f"       Cлоты для записи:  {schedule.slot_time} минут\n" \
-           f"Близжайший свободный слот ---?\n" \
-           f"Мои записи на этом объекте"
+
+    # TODO: предполагаю, что ответ состоит из 4-х частей
+    # TODO: 1-я часть - инфо про сам объект
+    # TODO: 2-я часть - итерационно собирается из расписаний и текст в зависимости от их типа слотов - разрешающих или запрещающих
+    # TODO: Время работы - для разрешающего
+    # TODO: Время перерыва - для запрещающего
+    # TODO: 3-я часть - данные про слоты
+    # TODO: 4-я часть данные про записи пользователя на этом объекте
+    # TODO: в конце их просто контакенировать
+
+    return (
+        f"Наименование: {rental.name}\n"
+        f"Описание: {rental.description}\n"
+        f"Расписание:\n"
+        f"       Время работы: {schedule.started} - {schedule.ended}\n"
+        f"       Cлоты для записи:  {schedule.slot_time} минут\n"
+        f"Близжайший свободный слот ---?\n"
+        f"Мои записи на этом объекте"
+    )
 
 
 def display_rental_slots(slot: SlotData) -> str:
     slot_date = slot.started.strftime("%d.%m.%Y г.")
     slot_start_time = slot.started.strftime("%H:%M")
     slot_end_time = slot.ended.strftime("%H:%M")
-    return f" Дата: {slot_date}\n" \
-           f" Время: {slot_start_time} - {slot_end_time}"
+    return f" Дата: {slot_date}\n" f" Время: {slot_start_time} - {slot_end_time}"
+
 
 # TODO здесь выводить описание политики бронирования
 def display_booking_info(schedule: Schedule) -> str:
     return f"Описание брониования: {schedule.description}"
 
-help_message = f"Краткая инструкция по использованию ботом.\n" \
-               f"Ссылка на канал с видеоинструкциями.\n" \
-               f"Ссылка на техподдержку."
+
+help_message = (
+    f"Краткая инструкция по использованию ботом.\n"
+    f"Ссылка на канал с видеоинструкциями.\n"
+    f"Ссылка на техподдержку."
+)
