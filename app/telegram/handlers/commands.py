346c38264a9fefa3ee64e82b5538ab3e7e22c183
aa.bezkrovnyy <aa.bezkrovnyy@jet.su>
2024-01-11 06:35:41 +0300

    remove magic "private", add aiogram.enums.chat_type

--- app/telegram/handlers/commands.py
+++ app/telegram/handlers/commands.py
@@ -1,11 +1,12 @@
-from aiogram import Router, F
+from aiogram import Router, F, types
+from aiogram import filters
 from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, MEMBER, KICKED
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, ChatMemberUpdated
 from aiogram.fsm.context import FSMContext
 from loguru import logger
 from sqlalchemy.future import select
-
+from aiogram.enums.chat_type import ChatType
 from app.domain.helpers.enums import UserRole, UserStatus
 from app.infra.db.models.user.schema import User
 from app.telegram.utils.db_queries import (
@@ -20,7 +21,7 @@ from app.telegram.messages.text_messages import (
 from app.telegram.keyboards.regular_user_kb import create_first_regular_keyboard
 
 router: Router = Router()
-router.my_chat_member.filter(F.chat.type == "private")
+router.my_chat_member.filter(F.chat.type == ChatType.PRIVATE)
 
 
 @router.message(CommandStart())
@@ -48,7 +49,9 @@ async def process_start_command(message: Message, db_session, state: FSMContext)
     if UserRole.REGULAR in UserRole(user.roles):
         available_rentals: int = await get_rentals_for_user_count(db_session=db_session)
         total_rentals = available_rentals
-        records_amount: int = await get_records_for_user_count(db_session, message.from_user.id)
+        records_amount: int = await get_records_for_user_count(
+            db_session, message.from_user.id
+        )
         await message.answer(
             hello_regular_user(
                 message.from_user.username,
